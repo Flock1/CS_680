be91bc50fd75e77bb43609338abed454921dfbd0
Flock-Anizak <sarvagya.gupta18@gmail.com>
2021-12-20 22:40:59 -0500

    Add files via upload

---
+++ HW9/test/src/hw09/filesystem/FileSystemTest.java
@@ -0,0 +1,64 @@
+package hw09.filesystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import hw09.apfs.apfs;
+import hw09.apfs.apfsDirectory;
+import hw09.apfs.apfsFile;
+import hw09.apfs.apfsLink;
+import hw09.apfs.apfs;
+import hw09.apfs.apfsDirectory;
+
+class FileSystemTest {
+	static LocalDateTime localTime = LocalDateTime.now();
+	
+	@SuppressWarnings("unused")
+	@BeforeAll
+	public static void setupupoffilestructure() {
+		
+		apfs FilesystemApfs = apfs.getAPFSFileSystem();
+		apfsDirectory root = (apfsDirectory)FilesystemApfs.initFS("drive", 3500);
+		apfsDirectory applications = new apfsDirectory("applications", root, 0, localTime, "sarvagya", localTime);
+		apfsDirectory home = new apfsDirectory("home", root, 0, localTime, "sarvagya", localTime);
+		apfsDirectory code = new apfsDirectory("code", home, 0, localTime, "sarvagya", localTime);
+		apfsDirectory pics = new apfsDirectory("pics", home, 0, localTime, "sarvagya", localTime);
+		apfsFile a = new apfsFile(applications, "a", 350, localTime, "sarvagya", localTime);
+		apfsFile b = new apfsFile(home, "b", 700, localTime, "sarvagya", localTime);
+		apfsFile c = new apfsFile(code, "c", 800, localTime, "sarvagya", localTime);
+		apfsFile d = new apfsFile(code, "d", 80, localTime, "sarvagya", localTime);
+		apfsFile e = new apfsFile(pics, "e", 700, localTime, "sarvagya", localTime);
+		apfsFile f = new apfsFile(pics, "f", 870, localTime, "sarvagya", localTime);
+	}
+	@Test
+	public void testforrootinstance() {
+		apfs fileSystemApfs = apfs.getAPFSFileSystem();
+		apfsDirectory root = (apfsDirectory) fileSystemApfs.getRoot();
+		assertSame(root.getCurrentname(), "sarvagya");
+	}
+	
+	private String[] dirToStringArray(apfsDirectory dir) {
+		Optional<apfsDirectory> optionalDirectory = Optional.ofNullable(dir.getParent());
+		String[] infofordirectory = { Boolean.toString(dir.isDirectory()), dir.getName(), Integer.toString(dir.getSize()),  
+				optionalDirectory.isPresent()?dir.getParent().getName():null, Integer.toString(dir.getTotalSize()),
+						Integer.toString(dir.countChildren()),dir.getCurrentname()};
+		return infofordirectory;
+	}
+	
+	@Test
+	public void RootCheck() {
+		apfs FileSystemApfs = apfs.getAPFSFileSystem();
+		apfsDirectory root = (apfsDirectory)FileSystemApfs.getRoot();
+		String[] expected = { "true", "root", "0", null, "3500", "2", "sarvagya"};
+		System.out.println(root.findDirectory("root"));
+		apfsDirectory actual = root.findDirectory("root");
+		assertArrayEquals(expected,dirToStringArray(actual));
+	}
+	
+
+}
